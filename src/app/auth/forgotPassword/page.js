2cdea4f971f388f8d792eecabea0e17f2ec8621c
Prashant Joshi <prashantjoshipj03@gmail.com>
2025-06-07 21:15:59 +0530

    issue Fixed

--- src/app/auth/forgotPassword/page.js
+++ src/app/auth/forgotPassword/page.js
@@ -1,65 +1,70 @@
 "use client";
-import {callApi} from '@/Common/callApi';
-import Toast, { playSound } from '@/Common/ToastMessage';
-import { CircularProgress } from '@mui/material';
-import { useRouter } from 'next/navigation';
-import React from 'react'
+import { callApi } from "@/Common/callApi";
+import Toast, { playSound } from "@/Common/ToastMessage";
+import { CircularProgress } from "@mui/material";
+import { useRouter } from "next/navigation";
+import React from "react";
 function ForgotPassword() {
-    const [email,setEmail]=React.useState()
-    const [emailError,setEmailError]=React.useState(false)
-    const [loader,setLoader]=React.useState(false)
+  const [email, setEmail] = React.useState();
+  const [emailError, setEmailError] = React.useState(false);
+  const [loader, setLoader] = React.useState(false);
 
-    const router=useRouter()
-    const submitData = async()=>{
-        try{
-            setLoader(true)
+  const router = useRouter();
+  const submitData = async () => {
+    try {
+      setLoader(true);
 
-        if(!email){
-            setEmailError(true)
-            setLoader(false)
+      if (!email) {
+        setEmailError(true);
+        setLoader(false);
+      } else {
+        const response = await callApi("auth/forgot-password", { email });
+        if (response.status === "success") {
+          Toast.success(response.message);
+          playSound();
+          setLoader(false);
+          router.push("/auth/login");
+        } else {
+          Toast.error(response.message);
+          playSound();
+          setLoader(false);
         }
-        else{
-            const response= await callApi("auth/forgot-password",{email})
-            if(response.status==="success"){
-                 Toast.success(response.message);
-                 playSound();
-            setLoader(false)
-
-                 router.push("/auth/login")
-                 
-            }
-            else{
-                Toast.error(response.message);
-                 playSound();
-            setLoader(false)
-
-            }
-        }
-        }
-        catch(error){
-                 Toast.error(error);
-                 playSound();
-            setLoader(false)
-
-        }
-}
-    function changeEmail(event){
-        setEmail(event.target.value)
-        setEmailError(false)
+      }
+    } catch (error) {
+      Toast.error(error);
+      playSound();
+      setLoader(false);
     }
+  };
+  function changeEmail(event) {
+    setEmail(event.target.value);
+    setEmailError(false);
+  }
   return (
-      <div className='h-[100%] overflow-hidden w-full flex justify-center items-center relative'>
-      <div className='w-[80%] max-w-[500px]'>
-        <span className='w-full flex justify-center text-2xl lg:text-3xl p-5'>Please enter the Email</span>
-       <label>Email</label>
-        <input className='h-[50px] pl-5 w-full mt-2 mb-1 rounded-2xl border-2 border-[#40BDEE]' type="email" value={email} onChange={changeEmail}  />
-        {emailError && <span className='text-[red] text-[14px]'>Email is required</span>}
-        <button onClick={submitData} className='w-full p-2 bg-[#40BDEE] text-[black] rounded-2xl mt-[100px] text-2xl'> 
-            {loader ? <CircularProgress style={{ color: "white" }} /> :"Submit"} </button>
+    <div className="h-[100%] overflow-hidden w-full flex justify-center items-center relative">
+      <div className="w-[80%] max-w-[500px]">
+        <span className="w-full flex justify-center text-2xl lg:text-3xl p-5">
+          Please enter the Email
+        </span>
+        <label>Email</label>
+        <input
+          className="h-[50px] pl-5 w-full mt-2 mb-1 rounded-2xl border-2 border-[#40BDEE]"
+          type="email"
+          value={email}
+          onChange={changeEmail}
+        />
+        {emailError && (
+          <span className="text-[red] text-[14px]">Email is required</span>
+        )}
+        <button
+          onClick={submitData}
+          className="w-full p-2 bg-[#40BDEE] text-[black] rounded-2xl mt-[100px] text-2xl"
+        >
+          {loader ? <CircularProgress style={{ color: "white" }} /> : "Submit"}{" "}
+        </button>
       </div>
-       </div>
-  )
+    </div>
+  );
 }
 
-export default ForgotPassword
-
+export default ForgotPassword;
--- src/app/auth/generatePassword/page.js
+++ src/app/auth/generatePassword/page.js
@@ -1,23 +1,37 @@
 "use client";
 import {callApi} from '@/Common/callApi';
+import Toast, { playSound } from '@/Common/ToastMessage';
+import { CircularProgress } from '@mui/material';
+import { useRouter, useSearchParams } from 'next/navigation';
 import React from 'react'
 function GeneratePassword() {
     const [password,setPassword]=React.useState()
+    const [loader,setLoader]=React.useState()
     const [passwordError,setPasswordError]=React.useState(false)
-
-    const submitData = ()=>{
+    const pathname = useSearchParams()
+    const code = pathname.get("code")
+    const router= useRouter()
+    const submitData = async()=>{
+        setLoader(true);
         if(!password){
             setPasswordError(true)
+            setLoader(false);
+            
         }
         else{
-            const response= callApi("auth/generate-password",{password})
-            console.log("res",response);
+            const response= await callApi(`auth/generate-password?code=${code}`,{password})
+            Toast.error(response.message);
+            playSound();
+            setLoader(false);
+            if(response.status==="success"){
+                router.push("/auth/login");
+            }
         }
       
     }
     function changeEmail(event){
-        setEmail(event.target.value)
-        setEmailError(false)
+        setPassword(event.target.value)
+        setPasswordError(false)
     }
   return (
       <div className='h-[100%] overflow-hidden w-full flex justify-center items-center relative'>
@@ -26,7 +40,9 @@ function GeneratePassword() {
        <label>New Password</label>
         <input className='h-[50px] pl-5 w-full mt-2 mb-1 rounded-2xl border-2 border-[#40BDEE]' type="text" value={password} onChange={changeEmail}  />
         {passwordError && <span className='text-[red] text-[14px]'>Please enter new password</span>}
-        <button onClick={submitData} className='w-full p-2 bg-[#40BDEE] text-[black] rounded-2xl mt-[100px] text-2xl'> Submit </button>
+        <button onClick={submitData} className='w-full p-2 bg-[#40BDEE] text-[black] rounded-2xl mt-[100px] text-2xl'> 
+            {loader ? <CircularProgress style={{ color: "white" }} /> : "Submit"}{" "}
+        </button>
       </div>
        </div>
   )
